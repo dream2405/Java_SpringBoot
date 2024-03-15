3d044358c3629fc349a62a48bd940cb3f043e77a
Wonuk Kim <dream2405@naver.com>
2024-03-15 14:00:28 +0900

    첫번째 커밋

---
+++ src/main/java/org/example/class_project/ClassProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.class_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassProjectApplication.class, args);
+    }
+
+}
